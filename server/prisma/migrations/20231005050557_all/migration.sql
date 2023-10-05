50c5f86bbe9e4d4b7d267e539f42987dd680b579
Mehedi Hasan <mehedi08h@gmail.com>
2023-10-05 11:08:56 +0600

    create schema for follow,whitelist,cupon code, event

---
+++ server/prisma/migrations/20231005050557_all/migration.sql
@@ -0,0 +1,252 @@
+/*
+  Warnings:
+
+  - The `avatarUrl` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN', 'SELLER');
+
+-- CreateEnum
+CREATE TYPE "AddressType" AS ENUM ('HOME', 'OFFICE', 'VARSITY');
+
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('PROCESSING', 'SHIPPED', 'DELIVERED', 'CANCELLED', 'RETURN');
+
+-- CreateEnum
+CREATE TYPE "EventStatus" AS ENUM ('RUNNING', 'ENDED', 'PROCESSING', 'STOP');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "active" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "phone" TEXT,
+ADD COLUMN     "resetPasswordExpire" TIMESTAMP(3),
+ADD COLUMN     "resetPasswordToken" TEXT,
+ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'USER',
+DROP COLUMN "avatarUrl",
+ADD COLUMN     "avatarUrl" JSONB;
+
+-- CreateTable
+CREATE TABLE "Shop" (
+    "id" SERIAL NOT NULL,
+    "shopName" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "logoUrl" JSONB,
+    "coverUrl" JSONB,
+    "images" JSONB,
+    "mails" TEXT NOT NULL,
+    "phones" JSONB NOT NULL,
+    "socialLinks" JSONB NOT NULL,
+    "totalSell" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "returnPolicy" INTEGER NOT NULL DEFAULT 7,
+    "moneyBackPolicy" BOOLEAN NOT NULL DEFAULT true,
+    "verifiedAt" TIMESTAMP(3),
+    "secret" TEXT NOT NULL,
+    "positiveRating" INTEGER NOT NULL DEFAULT 100,
+    "shipOnTime" INTEGER NOT NULL DEFAULT 100,
+    "chatResponse" INTEGER NOT NULL DEFAULT 100,
+    "shopType" TEXT NOT NULL,
+    "shopCategory" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT false,
+    "block" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "resetSecretToken" TEXT,
+    "resetSecretExpire" TIMESTAMP(3),
+
+    CONSTRAINT "Shop_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Follow" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "shopId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Follow_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WhiteList" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "WhiteList_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" SERIAL NOT NULL,
+    "country" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "address1" TEXT NOT NULL,
+    "address2" TEXT,
+    "zipCode" TEXT,
+    "addressType" "AddressType" NOT NULL DEFAULT 'HOME',
+    "userId" INTEGER NOT NULL,
+    "shopId" INTEGER NOT NULL,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "image" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "salePrice" DOUBLE PRECISION NOT NULL,
+    "discount" INTEGER NOT NULL DEFAULT 0,
+    "images" JSONB NOT NULL,
+    "rating" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "stock" INTEGER NOT NULL DEFAULT 1,
+    "soldOut" INTEGER NOT NULL DEFAULT 0,
+    "freeDelivery" BOOLEAN NOT NULL DEFAULT false,
+    "coin" INTEGER NOT NULL DEFAULT 0,
+    "warranty" BOOLEAN NOT NULL DEFAULT true,
+    "warrantyDuration" INTEGER NOT NULL DEFAULT 0,
+    "highlight" JSONB,
+    "specitications" JSONB,
+    "categoryId" INTEGER NOT NULL,
+    "colors" JSONB NOT NULL,
+    "orderId" INTEGER,
+    "variants" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" SERIAL NOT NULL,
+    "description" TEXT NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "rating" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "paymentIntent" TEXT NOT NULL,
+    "paymentStatus" BOOLEAN NOT NULL DEFAULT false,
+    "itemsPrice" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "taxPrice" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "shippingPrice" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "totalPrice" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "status" "OrderStatus" NOT NULL DEFAULT 'PROCESSING',
+    "userId" INTEGER NOT NULL,
+    "shopId" INTEGER NOT NULL,
+    "paidAt" TIMESTAMP(3),
+    "deliveredAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "images" JSONB NOT NULL,
+    "discount" INTEGER NOT NULL DEFAULT 0,
+    "categoryId" INTEGER NOT NULL,
+    "status" "EventStatus" NOT NULL DEFAULT 'PROCESSING',
+    "startedAt" TIMESTAMP(3) NOT NULL,
+    "endedAt" TIMESTAMP(3) NOT NULL,
+    "shopId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CoupounCode" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "value" INTEGER NOT NULL DEFAULT 0,
+    "minAmount" INTEGER NOT NULL DEFAULT 0,
+    "maxAmount" INTEGER DEFAULT 0,
+    "startedAt" TIMESTAMP(3) NOT NULL,
+    "endedAt" TIMESTAMP(3) NOT NULL,
+    "shopId" INTEGER NOT NULL,
+    "userId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CoupounCode_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Shop_mails_key" ON "Shop"("mails");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Address_address2_key" ON "Address"("address2");
+
+-- AddForeignKey
+ALTER TABLE "Follow" ADD CONSTRAINT "Follow_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follow" ADD CONSTRAINT "Follow_shopId_fkey" FOREIGN KEY ("shopId") REFERENCES "Shop"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WhiteList" ADD CONSTRAINT "WhiteList_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WhiteList" ADD CONSTRAINT "WhiteList_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_shopId_fkey" FOREIGN KEY ("shopId") REFERENCES "Shop"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_shopId_fkey" FOREIGN KEY ("shopId") REFERENCES "Shop"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_shopId_fkey" FOREIGN KEY ("shopId") REFERENCES "Shop"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CoupounCode" ADD CONSTRAINT "CoupounCode_shopId_fkey" FOREIGN KEY ("shopId") REFERENCES "Shop"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CoupounCode" ADD CONSTRAINT "CoupounCode_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
